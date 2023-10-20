639cce44df02b75ed56bd2a98b7dbc14e08df886
Miłosz Mazur <milosz.mazur@jeppesen.com>
2023-10-20 11:16:47 +0200

    nvim: configure find_files

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -4,3 +4,20 @@ vim.keymap.set('n', '<leader>/', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>b', builtin.buffers, {})
 vim.keymap.set('n', '<leader>ht', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>hk', builtin.keymaps, {})
+
+require('telescope').setup {
+  pickers = {
+    find_files = {
+      hidden = true,
+      find_command = {
+        "fd",
+        ".",
+        "--type",
+        "file",
+        "--hidden",
+        "--strip-cwd-prefix",
+        "--exclude", ".git/"
+      },
+    }
+  }
+}
