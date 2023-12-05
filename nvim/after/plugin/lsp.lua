150208339229e5e399d6cb9fbd879b4b128046fe
Miłosz Mazur <milosz.mazur@hotmail.com>
2023-12-05 10:36:01 +0100

    nvim: accept suggestion on return

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -25,6 +25,7 @@ cmp.setup({
   mapping = {
     ['<Tab>'] = cmp_action.luasnip_supertab(),
     ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+    ['<CR>'] = cmp.mapping.confirm({ select = false }),
   },
   sources = {
     { name = 'nvim_lsp' },
