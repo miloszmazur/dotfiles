9da18b5d7ad1328019dd5260cf3b2c2274491db5
Miłosz Mazur <milosz.mazur@jeppesen.com>
2025-07-03 21:02:48 +0200

    update osx_defaults

--- osx_defaults.sh
+++ osx_defaults.sh
@@ -10,12 +10,6 @@ chflags nohidden ~/Library
 # Set a really fast key repeat.
 defaults write NSGlobalDomain KeyRepeat -int 2
 
-# check for software updates daily, not just once per week
-defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
-
-# disable the sudden motion sensor as it’s not useful for SSDs
-sudo pmset -a sms 0
-
 # show hidden files by default
 defaults write com.apple.finder AppleShowAllFiles -bool true
 
@@ -34,9 +28,6 @@ defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 # Disable the warning before emptying the Trash
 defaults write com.apple.finder WarnOnEmptyTrash -bool false
 
-# Empty Trash securely by default
-defaults write com.apple.finder EmptyTrashSecurely -bool true
-
 # Show the main window when launching Activity Monitor
 defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
 
@@ -50,15 +41,9 @@ defaults write com.apple.ActivityMonitor ShowCategory -int 0
 defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
 defaults write com.apple.ActivityMonitor SortDirection -int 0
 
-# Disable automatic emoji substitution (i.e. use plain text smileys)
-defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "automaticEmojiSubstitutionEnablediMessage" -bool false
-
 # Hide all files from desktop unless viewed from Finder
 # change to true to reverse changes
 defaults write com.apple.finder CreateDesktop -bool false
-
-# use bacspace to go back in Safari
-defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2BackspaceKeyNavigationEnabled -bool YES
 
 # save screenshots to iCloud
 defaults write com.apple.screencapture location ~/Library/Mobile\ Documents/com~apple~CloudDocs/Screenshots
